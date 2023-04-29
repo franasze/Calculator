c33911c83ad1fd8e038924682cb21289d1a1347a
Arkadiusz <franasze@student.wszib.edu.pl>
2023-04-29 17:27:31 +0200

    not finished

--- js/main.js
+++ js/main.js
@@ -1,80 +1,67 @@
-console.log("Hi!");
-
-// console.log(1 == "1"); true
-// console.log(1 === "1"); false
-
-// document.querySelector("#plus-button").addEventListener("click", () =>{
-//   const resultElement = document.querySelector("#resault");
-//   resultElement.innerText = parseFloat(resultElement.innerText) + 1;
-// });
-
-
-function  numberClickListiner(number) {
+function numberClickListener(char) {
   return () => {
-    const resultElement = document.querySelector("#number");
-    if (resultElement.innerText === '0') {
-      resultElement.innerText = number;
+    const element = document.querySelector("#number");
+    if(element.innerText === '0') {
+      element.innerText = char;
     } else {
-      resultElement.innerText = resultElement.innerText + number;
+      element.innerText = element.innerText + char;
     }
   };
 }
-document.querySelector("#one-button").addEventListener("click",numberClickListiner(1));
-document.querySelector("#two-button").addEventListener("click",numberClickListiner(2));
-document.querySelector("#three-button").addEventListener("click",numberClickListiner(3));
-document.querySelector("#four-button").addEventListener("click",numberClickListiner(4));
-document.querySelector("#five-button").addEventListener("click",numberClickListiner(5));
-document.querySelector("#six-button").addEventListener("click",numberClickListiner(6));
-document.querySelector("#seven-button").addEventListener("click",numberClickListiner(7));
-document.querySelector("#eight-button").addEventListener("click",numberClickListiner(8));
-document.querySelector("#nine-button").addEventListener("click",numberClickListiner(9));
-document.querySelector("#zero-button").addEventListener("click",numberClickListiner(0));
 
-document.querySelector("#dot-button").addEventListener("click", ()=>{
-  const Element = document.querySelector("#number");
-  if(Element.innerText.indexOf('.') === -1){
-    Element.innerText = Element.innerText + '.';
+document.querySelector("#one-button").addEventListener("click", numberClickListener(1));
+document.querySelector("#two-button").addEventListener("click", numberClickListener(2));
+document.querySelector("#three-button").addEventListener("click", numberClickListener(3));
+document.querySelector("#four-button").addEventListener("click", numberClickListener(4));
+document.querySelector("#five-button").addEventListener("click", numberClickListener(5));
+document.querySelector("#six-button").addEventListener("click", numberClickListener(6));
+document.querySelector("#seven-button").addEventListener("click", numberClickListener(7));
+document.querySelector("#eight-button").addEventListener("click", numberClickListener(8));
+document.querySelector("#nine-button").addEventListener("click", numberClickListener(9));
+document.querySelector("#zero-button").addEventListener("click", numberClickListener(0));
+
+document.querySelector("#dot-button").addEventListener("click", () => {
+  const element = document.querySelector("#number");
+  if(element.innerText.indexOf('.') === -1) {
+    element.innerText = element.innerText + '.';
   }
 });
 
-document.querySelector("#remove-last-char-button").addEventListener("click", () => {
-  const Element = document.querySelector("#number");
-  element.innerText.substring(0,element.innerText.length - 1);
+document.querySelector("#CE-button").addEventListener("click", () => {
+  const element = document.querySelector("#number");
+  element.innerText = element.innerText.substring(0, element.innerText.length - 1);
 
-  if(element.innerText.length === 0){
+  if(element.innerText.length === 0) {
     element.innerText = '0';
   }
-
 });
 
-  document.querySelector("#remove-all-button").addEventListener("click", () => {
-    document.querySelector("#number").innerText = '0';
-    document.querySelector("#number").innerText = '0';
-
+document.querySelector("#C-button").addEventListener("click", () => {
+  document.querySelector("#result").innerText = '0';
+  document.querySelector("#number").innerText = '0';
 });
 
-
-
-document.querySelector("#plus-button").addEventListener("click", () => {
-  const resultElement = document.querySelector("#result");
-  const numberElement = document.querySelector("#number");
-
-  resultElement.innerText = parseFloat(resultElement.innerText) - parseFloat(numberClickListiner())
-  numberElement.innerHTML = '0';
+document.querySelector("#enter-button").addEventListener("click", () => {
+  document.querySelector("#result").innerText = document.querySelector("#number").innerText;
+  document.querySelector("#number").innerText = '0';
 });
 
-function operationClickListener(operation){
+function operationClickListener(operation) {
   return () => {
-  const resultElement = document.querySelector("#result");
-  const numberElement = document.querySelector("#number");
+    const resultElement = document.querySelector("#result");
+    const numberElement = document.querySelector("#number");
 
-  resultElement.innerText = Mth.round(operation(parseFloat(resultElement.innerText),parseFloat(numberElement.innerText *1000) / 1000
-  numberElement.innerHTML = '0';
+
+      resultElement.innerText = Math.round(operation(parseFloat(resultElement.innerText), parseFloat(numberElement.innerText)) * 100000) / 100000;
+      numberElement.innerText = '0';
+
   }
 }
 
-document.querySelector("#plus-button").addEventListener("click",operationClickListener((a,b) => a+b));
-document.querySelector("#minus-button").addEventListener("click",operationClickListener((a,b) => a-b));
-document.querySelector("#mulitply-button").addEventListener("click",operationClickListener((a,b) => a*b);
-document.querySelector("#divide-button").addEventListener("click",operationClickListener((a,b) => a/b));
+document.querySelector("#plus-button").addEventListener("click", operationClickListener((a, b) => a + b));
+document.querySelector("#minus-button").addEventListener("click", operationClickListener((a, b) => a - b));
+document.querySelector("#multiply-button").addEventListener("click", operationClickListener((a, b) => a * b));
+document.querySelector("#divide-button").addEventListener("click", operationClickListener((a, b) => a / b));
+document.querySelector("#percent-button").addEventListener("click", operationClickListener((a, b) => a * (b / 100)));
+
 
